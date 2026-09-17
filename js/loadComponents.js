3701c7f9891fde252412daf2f45cc7556fd732e3
samimosmansabuj <samim.o.sabuj01@gmail.com>
2026-09-18 02:07:46 +0600

    update all assets file link!!!

--- js/loadComponents.js
+++ js/loadComponents.js
@@ -51,7 +51,7 @@ async function initComponents() {
     // Ensure cart-drawer.js is loaded
     if (!window.openCartDrawer && !document.querySelector('script[src*="cart-drawer.js"]')) {
         const cdScript = document.createElement("script");
-        cdScript.src = "js/cart-drawer.js?v=211";
+        cdScript.src = "js/cart-drawer.js";
         document.body.appendChild(cdScript);
     }
 
